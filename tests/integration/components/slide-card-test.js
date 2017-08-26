c02c31abe1532be8f5119a23c8fd65f3a8990bb1
Chris Hayes <chris@moderncognition.com>
2017-08-26 10:13:52 -0400

    Add slide-card component tests

--- tests/integration/components/slide-card-test.js
+++ tests/integration/components/slide-card-test.js
@@ -19,7 +19,25 @@ moduleForComponent('slide-card', 'Integration | Component | single slide', {
   }
 });
 
-test('it renders', function(assert) {
+test('it registers and unregisters itself with the slide deck with the correct name', function (assert) {
+  assert.expect(2);
+
+  this.on('registerSlide', name => assert.equal(name, 'test'));
+  this.on('unregisterSlide', name => assert.equal(name, 'test'));
+
+  this.render(hbs`
+    {{#slide-card
+      tagName="div"
+      name="test"
+      register-slide=(action "registerSlide")
+      unregister-slide=(action "unregisterSlide")
+    }}
+      template block text
+    {{/slide-card}}
+  `);
+});
+
+test('it does not render its content when it is not active', function (assert) {
   this.render(hbs`
     {{#slide-card
       tagName="div"
@@ -31,4 +49,35 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(component.text, '');
+});
+
+test('it renders the inverse content when it is not active', function (assert) {
+  this.render(hbs`
+    {{#slide-card
+      tagName="div"
+      register-slide=(action "registerSlide")
+      unregister-slide=(action "unregisterSlide")
+    }}
+      template block text
+    {{else}}
+      inverse template block text
+    {{/slide-card}}
+  `);
+
+  assert.equal(component.text, 'inverse template block text');
+});
+
+test('it renders its content when it is active', function (assert) {
+  this.render(hbs`
+    {{#slide-card
+      tagName="div"
+      isActive=true
+      register-slide=(action "registerSlide")
+      unregister-slide=(action "unregisterSlide")
+    }}
+      template block text
+    {{/slide-card}}
+  `);
+
+  assert.equal(component.text, 'template block text');
 });
